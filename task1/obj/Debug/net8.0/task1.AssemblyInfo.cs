bf6d709b858427e933fd0703e82a797946828a57
yarina golubkova <yarina-druc@mail.ru>
2024-01-24 18:24:07 +0300

    Dobavila vidy algoritmovД

--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df1b405245191bed3fc46448e433df183628b2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9532de3a6bf7dd18956dc613f0c7dd536bab2947")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
